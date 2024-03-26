2ffe9646d8a9ceee1021c92ee26acf72b14535aa
Pham Huu Tri <trib2014624@student.ctu.edu.vn>
2024-03-27 01:40:31 +0700

    Cai dat route va testcase cho route root '/'

--- librarybooks-backend/app.js
+++ librarybooks-backend/app.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const rootUnsupportedMethods = ['post', 'put', 'delete', 'patch', 'options', 'head'];
+app.all('/', (req, res, next) => {
+    if (rootUnsupportedMethods.includes(req.method.toLowerCase())) {
+        return next(new ApiError(405, 'Unsupported method'));
+    } else {
+        next();
+    }
+});
+
 app.get("/", (req, res)=> {
     res.json({ message: "Welcom to librarybooks-backend" });
+});
+
+// app.use("/api/publishers", publicshersRouter);
+// app.use("/api/books", booksRouter);
+// app.use("/api/borrowcards", borrowcardsRouter);
+// app.use("/api/librarians", librariansRouter);
+// app.use("/app/students", studentsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
--- librarybooks-backend/testserver.js
+++ librarybooks-backend/testserver.js
@@ -10,14 +10,22 @@ describe('Test server routes', function(){
             assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
             assert.deepStrictEqual(response.body, { message: 'Welcom to librarybooks-backend' });
         });
-        const unsupportedMethods = ['post', 'put', 'delete', 'patch', 'options', 'head'];
+        const unsupportedMethods = ['post', 'put', 'delete', 'patch'];
         unsupportedMethods.forEach(function(method) {
-            it('should return JSON response with "Unsupported method" message for ${method.toUpperCase()} method request', async function(){
-                const response = await request(app).get('/');
+            it(`should return JSON response with "Unsupported method" message for ${method.toUpperCase()} method request`, async function(){
+                const response = await request(app)[method]('/');
                 assert.strictEqual(response.status, 405);
                 assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
                 assert.deepStrictEqual(response.body, { message: 'Unsupported method' });
             });
+        })
+        it ('HEAD should return JSON response with status 405', async function(){
+            const response = await request(app).head('/');
+            assert.strictEqual(response.status, 405);
+        })
+        it ('OPTIONS should return JSON response with status 204', async function(){
+            const response = await request(app).options('/');
+            assert.strictEqual(response.status, 204);
         })
     })
 })
